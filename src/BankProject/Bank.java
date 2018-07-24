6cb88f59d8766b09122792a62db3232106d6af02
HarveyShan <harveyshan918@gmail.com>
2018-07-24 12:01:36 -0700

    added user interface for siging up, no accounts opening and no User ID yet

--- src/BankProject/Bank.java
+++ src/BankProject/Bank.java
@@ -14,10 +14,12 @@ public class Bank {
 		System.out.println("Please Sign up  or Register");
 		System.out.println("1. Sign up");
 		System.out.println("2. Log In");
+		System.out.println("Press any other key to exit Charles Schwab");
 		System.out.println("-------------------------");
 		
 		Scanner in = new Scanner(System.in);
 		String choice = in.next();
+		
 		if(choice.equals("1"))
 		{
 			System.out.println("-------------------------");
@@ -26,15 +28,39 @@ public class Bank {
 			System.out.println("Please Choose A User Name: ");
 			String name = in.next();
 			System.out.println("------------------------->");
-			System.out.println("Please Enter An All Number Password: ");
-			int password = in.nextInt();
+			System.out.println("Please Enter A Password: ");
+			String password = in.next();
 			
 			System.out.println("<------------------------->");
+			System.out.println("Thank you for choosing Charles Schwabs");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 			System.out.println("You Currently Have Zero Accounts");
 			System.out.println("Would You Like To Open A New Account?");
+			System.out.println(" ");
 			System.out.println("1. Yes I Would Like To Open A New Account");
-			System.out.println("2. No I Want To Exit This Program Now");
+			System.out.println("Press any other key other than 1 to exit the program");
 		}
+		
+		else
+		{
+			System.out.println("Thank you for choosing Charles Schwab, have a great day");
+		}
+		
+		String userInput = in.next();
+		
+		if(userInput.equals("1"))
+		{
+			System.out.println("What Type Of Account Would You Like To Open?");
+			System.out.println("1.Checking");
+			System.out.println("2.Saving");
+		}
+		
+		else
+		{
+			System.out.println("Thank you for choosing Charles Schwab, have a great day");
+		}
+		
+		
 	}
 	
 	
